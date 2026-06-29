3d1b51d784147552bcdb71ad6561bc08fc6d5409
kota shivatarak reddy <161205634+shivatarak@users.noreply.github.com>
2026-06-29 18:11:05 +0530

    Time: 16 ms (19.87%), Space: 52.4 MB (90.68%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer>map=new HashMap<>();
+        int major=nums.length/2;
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+            if(map.get(num)>major){
+                return num;
+            }
+        }
+        return -1;
+    }
+}
