0c2fafe3848b13cdee6d226c23af1ea6b7cc5d02
kota shivatarak reddy <161205634+shivatarak@users.noreply.github.com>
2026-08-18 00:22:34 +0530

    Time: 1 ms (38.02%), Space: 43.2 MB (86.05%) - LeetHub

---
+++ 3884-first-matching-character-from-both-ends/3884-first-matching-character-from-both-ends.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int firstMatchingIndex(String s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s.charAt(i)==s.charAt(j)){
+                return i;
+            }
+            i++;
+            j--;
+        }
+        return -1;
+    }
+}
