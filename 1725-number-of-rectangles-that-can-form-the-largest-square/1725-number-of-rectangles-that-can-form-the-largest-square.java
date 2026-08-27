f37debbcd3e607b3ba255a636168d5ad2fa4c415
kota shivatarak reddy <161205634+shivatarak@users.noreply.github.com>
2026-08-27 12:15:42 +0530

    Time: 9 ms (10.66%), Space: 46.8 MB (78.31%) - LeetHub

---
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int countGoodRectangles(int[][] rectangles) {
+        int n=rectangles.length;
+        int max=0;
+        HashMap<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<n;i++){
+                int first=rectangles[i][0];
+                int second=rectangles[i][1];
+                int min=Math.min(rectangles[i][0],rectangles[i][1]);
+                max=Math.max(max,min);
+                map.put(min,map.getOrDefault(min,0)+1);
+        }
+        return map.get(max);
+    }
+}
