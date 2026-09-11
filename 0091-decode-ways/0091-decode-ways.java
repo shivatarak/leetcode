595ebaf55b2e43fd3cccc51326ee69dbad1be843
kota shivatarak reddy <161205634+shivatarak@users.noreply.github.com>
2026-09-11 22:25:19 +0530

    Time: 1 ms (81.3%), Space: 43.5 MB (7.48%) - LeetHub

---
+++ 0091-decode-ways/0091-decode-ways.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n=s.length();
+        int dp[]=new int[n+1];
+        dp[0]=1;
+        dp[1]=s.charAt(0)=='0'?0:1;
+        for(int i=2;i<=n;i++){
+            int oned=Integer.valueOf(s.substring(i-1,i));
+            int twod=Integer.valueOf(s.substring(i-2,i));
+            if(oned>=1){
+                dp[i]+=dp[i-1];
+            }
+            if(twod>=10 && twod<=26){
+                dp[i]+=dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
